ccad4160d5cb1c7bf0e049644f8c0848bb1a52fa
Anthony Rentsch <anthony.rentsch@gmail.com>
2018-11-23 15:26:55 -0500

    pie chart working initially

--- js/clock.js
+++ js/clock.js
@@ -17,27 +17,47 @@ Clockchart = function(_parentElement, _data) {
 Clockchart.prototype.initVis = function() {
     var vis = this;
 
-    // first cut using generic params from here: http://bl.ocks.org/enjalot/1203641
-    vis.w = 300;
-    vis.h = 300;
-    vis.r = 100;
-    vis.color = d3.scaleOrdinal()
-        .range(["#98abc5", "#8a89a6", "#7b6888"]);
+    // SVG drawing area
+    vis.height = 1000;
+    vis.width = 1000;
+    vis.radius = 200;
+    vis.p = Math.PI * 2;
 
     vis.svg = d3.select(vis.parentElement)
         .append("svg")
-        .data(vis.data)
-        .attr("width", vis.w)
-        .attr("height", vis.h)
-        .append("g")
-        .attr("transform", "translate(" + vis.r + "," + vis.r + ")");
+        .attr("height", vis.height)
+        .attr("width", vis.width);
+
+    vis.color = d3.scaleLinear()
+        .range(["white", "red"]);
+
+
+    vis.g = vis.svg.append("g")
+        .attr("transform", "translate(200,200)");
 
     vis.arc = d3.arc()
-        .outerRadius(vis.r)
-        .innerRadius(0);
+        .innerRadius(0)
+        .outerRadius(vis.radius)
+        //.startAngle(0)
+        .endAngle(vis.p);
+
 
     vis.clock = d3.pie()
-        .value(function(d) { return d[vis.measure]; });
+        .sort(null)
+        .value(function(d) {
+            //return d.value
+            return d.angle;
+        });
+
+    vis.arcs = vis.g.selectAll(".arc")
+        .data(vis.clock(vis.data))
+        .enter()
+        .append("g")
+        .attr("class", "arc")
+        .style('stroke','white')
+
+
+
 
     // call wrangleData
     vis.wrangleData();
@@ -47,11 +67,9 @@ Clockchart.prototype.initVis = function() {
 Clockchart.prototype.wrangleData = function() {
     var vis = this;
 
-    vis.arcs = vis.svg.selectAll("slice")
-        .data(vis.clock)
-        .enter()
-        .append("g")
-        .attr("class", "slice");
+    // Update color domain
+    vis.maxForColorScale = d3.max(vis.data, function(d) { return d[vis.measure]; });
+    vis.color.domain([0, vis.maxForColorScale]);
 
     // call updateVis
     vis.updateVis();
@@ -62,8 +80,10 @@ Clockchart.prototype.updateVis = function() {
     var vis = this;
 
     vis.arcs.append("path")
-        .attr("fill", function(d, i) { return vis.color(i); } )
-        .attr("d", vis.arc);
+        .attr("d", vis.arc)
+        .style("fill", function(d) {
+            return vis.color(d.data.HeroinCrimes);
+        });
 };
 
 
--- js/main.js
+++ js/main.js
@@ -2,12 +2,31 @@
 // data/NewUpdatedData/DrugsByhour.csv
 // ... /DrugsDAY_OF_WEEK.csv
 
-var clockChart;
+// see final response here:
+// https://stackoverflow.com/questions/35608105/how-to-divide-donut-chart-into-fixed-portion
+
+var clockChartMorning;
+var clockChartAfternoon;
 
 d3.csv("data/NewUpdatedData/DrugsByhour.csv", function(data) {
 
-    console.log(data);
+    data.forEach(function(d) {
+        d.hourofday = +d.hourofday;
+        d.HeroinCrimes = +d.HeroinCrimes;
+        d.WeedCrimes = +d.WeedCrimes;
+        d.angle = +d.angle;
+    });
 
-    clockChart = new Clockchart("#clock-chart", data);
+    var morning = [];
+    var afternoon = [];
+
+    for (var i = 0; i < data.length; i++) {
+        if (i < data.length/2) { morning.push(data[i]) }
+        else { afternoon.push(data[i]) }
+    }
+
+    clockChartMorning = new Clockchart("#clock-chart-morning", morning);
+
+    clockChartAfternoon = new Clockchart("#clock-chart-afternoon", afternoon)
 
 });
